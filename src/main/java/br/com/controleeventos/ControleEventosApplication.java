7fbf9f2e342169d762f4196a4cdbb0fe0711a62e
Leitesamara12 <samara_1bbs@hotmail.com>
2026-08-24 19:19:22 -0300

    Criar estrutura inicial do projeto

---
+++ src/main/java/br/com/controleeventos/ControleEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.controleeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controleeventos/ControleEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controleeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
